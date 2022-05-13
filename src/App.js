b42a62cebf8a5936f88b92c0c55681bab863c8c9
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-13 22:17:55 +0530

    Some minor adjustments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "react-toastify/dist/ReactToastify.css"
-import { toast,ToastContainer} from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import BuyPage from './Components/BuyPage';
-
-import {Container,Row,Col} from "reactstrap"
+import NavBar from './Components/Navbar';
+import { Container, Row, Col, Navbar } from "reactstrap"
 import Cart from "./Components/Cart"
 
 
@@ -12,7 +12,7 @@ function App() {
   const [cartItem, setCartItem] = useState([]);
 
   const addInCart = item => {
-    const isAlreadyAdded = cartItem.findIndex(function(array) {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
       return array.id !== item.id;
     });
 
@@ -39,17 +39,21 @@ function App() {
   };
 
   return (
-    <Container fluid>
-      <ToastContainer />
-      <Row>
-        <Col md="8">
-          <BuyPage addInCart={addInCart} />
-        </Col>
-        <Col md="4">
-          <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <NavBar />
+      <Container fluid>
+
+        <ToastContainer />
+        <Row>
+          <Col md="9">
+            <BuyPage addInCart={addInCart} />
+          </Col>
+          <Col md="3">
+            <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
--- src/Components/cartItem.js
+++ src/Components/cartItem.js
@@ -13,7 +13,7 @@ import {
 const CartItem = ({ product, addInCart }) => {
   return (
     <Card className="mt-2 mb-1">
-      <CardImg top height="250" width="100%" src={product.smallImage} />
+      <CardImg top height="200px" width="100%" src={product.smallImage} />
       <CardBody className="text-center">
         <CardTitle>{product.productName}</CardTitle>
         <CardText className="secondary">
